dcd857d630f03a62fdf1195378820f12ab3206cc
Mana Kampman <manastar04@gmail.com>
2021-11-17 14:21:41 -0800

    Initial Commit

---
+++ src/main/java/mitebemana/compatoplenty/core/CompatOPlenty.java
@@ -0,0 +1,23 @@
+package mitebemana.compatoplenty.core;
+
+import com.minecraftabnormals.abnormals_core.core.util.registry.RegistryHelper;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("compatoplenty")
+public class CompatOPlenty
+{
+    public static final String MOD_ID = "compatoplenty";
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final RegistryHelper REGISTRY_HELPER = new RegistryHelper(MOD_ID);
+
+    public CompatOPlenty() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        REGISTRY_HELPER.register(bus);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
